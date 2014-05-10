13e55b57bd64f1af9fe5bbd3deec7aa24d759944
yemonkyaw <yemonkyaw@d3bugg3rs.debuggers>
2014-05-10 02:02:07 -0400

    adding application class

---
+++ app/src/main/java/com/indexer/hellotaxi/app/Application/popa.java
@@ -0,0 +1,13 @@
+package com.indexer.hellotaxi.app.Application;
+
+import android.app.Application;
+
+import org.androidannotations.annotations.EApplication;
+
+/**
+ * Created by yemonkyaw on 5/10/14.
+ */
+@EApplication
+public class popa extends Application {
+
+}
