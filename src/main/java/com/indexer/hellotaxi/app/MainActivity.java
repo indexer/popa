1ff3d635f70d2ad6d1a3b62ae9cb36401df0da43
yemonkyaw <yemonkyaw@d3bugg3rs.debuggers>
2014-05-10 00:50:20 -0400

    for testing android annoation

---
+++ src/main/java/com/indexer/hellotaxi/app/MainActivity.java
@@ -0,0 +1,11 @@
+package com.indexer.hellotaxi.app;
+
+import android.support.v7.app.ActionBarActivity;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends ActionBarActivity {
+
+
+}
