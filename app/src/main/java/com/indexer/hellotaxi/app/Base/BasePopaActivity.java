58636b702803e248ba6dd1141421fcd26779d026
yemonkyaw <yemonkyaw@d3bugg3rs.debuggers>
2014-05-10 23:28:14 -0400

    add google map

--- app/src/main/java/com/indexer/hellotaxi/app/Base/BasePopaActivity.java
+++ app/src/main/java/com/indexer/hellotaxi/app/Base/BasePopaActivity.java
@@ -1,29 +1,41 @@
 package com.indexer.hellotaxi.app.Base;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.FragmentActivity;
 
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.indexer.hellotaxi.app.Application.popa;
 import com.indexer.hellotaxi.app.Module.ActivityModule;
+import com.indexer.hellotaxi.app.R;
 
 import java.util.Arrays;
 import java.util.List;
 
 import dagger.ObjectGraph;
 
-public abstract class BasePopaActivity extends ActionBarActivity {
+public abstract class BasePopaActivity extends FragmentActivity {
     private ObjectGraph activityGraph;
     private GoogleMap mMap;
 
+    protected int getLayoutId() {
+        return R.layout.activity_main;
+    }
 
     @Override
     protected void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
+        setContentView(getLayoutId());
         popa application = (popa) getApplication();
         activityGraph = application.getObjectGraph().plus(getModules().toArray());
         activityGraph.inject(this);
 
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
     }
 
     @Override
@@ -32,6 +44,26 @@ public abstract class BasePopaActivity extends ActionBarActivity {
         // soon as possible.
         activityGraph = null;
         super.onDestroy();
+    }
+
+    private void setUpMapIfNeeded() {
+        if (mMap != null) {
+            return;
+        }
+        mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+        if (mMap != null) {
+            startDemo();
+        }
+    }
+
+    /**
+     * Run the demo-specific code.
+     */
+    protected abstract void startDemo();
+
+    protected GoogleMap getMap() {
+        setUpMapIfNeeded();
+        return mMap;
     }
 
 
--- app/src/main/java/com/indexer/hellotaxi/app/ui/MainActivity.java
+++ app/src/main/java/com/indexer/hellotaxi/app/ui/MainActivity.java
@@ -2,6 +2,8 @@ package com.indexer.hellotaxi.app.ui;
 
 import android.view.Menu;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.model.LatLng;
 import com.indexer.hellotaxi.app.Base.BasePopaActivity;
 import com.indexer.hellotaxi.app.R;
 
@@ -10,6 +12,11 @@ import org.androidannotations.annotations.EActivity;
 @EActivity
 public class MainActivity extends BasePopaActivity {
 
+    @Override
+    protected void startDemo() {
+        getMap().moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(-33.8696, 151.2094), 10));
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
