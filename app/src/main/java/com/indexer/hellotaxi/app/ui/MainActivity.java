708c346837d80a3e7a43d355a8b08b5990599b60
yemonkyaw <yemonkyaw@d3bugg3rs.debuggers>
2014-05-11 06:01:09 -0400

    replace fragment with Activity

--- app/src/main/java/com/indexer/hellotaxi/app/ui/MainActivity.java
+++ app/src/main/java/com/indexer/hellotaxi/app/ui/MainActivity.java
@@ -1,13 +1,17 @@
 package com.indexer.hellotaxi.app.ui;
 
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.content.Context;
 import android.location.Location;
 import android.location.LocationManager;
 import android.view.Menu;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.maps.android.ui.IconGenerator;
 import com.indexer.hellotaxi.app.Application.popa_;
@@ -28,7 +32,6 @@ public class MainActivity extends BasePopaActivity {
     mlocationListener mlocationListener = new mlocationListener();
 
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
@@ -45,24 +48,34 @@ public class MainActivity extends BasePopaActivity {
     @UiThread
     protected void startActiviy() {
 
-        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        Location location = locationManager.getLastKnownLocation
+                (LocationManager.GPS_PROVIDER);
         Popa = (popa_) getApplicationContext();
         IconGenerator iconFactory = new IconGenerator(this);
         getMap().moveCamera(CameraUpdateFactory.newLatLngZoom
-                (new LatLng(location.getLatitude(), location.getLongitude()), 13));
+                (new LatLng(location.getLatitude(),
+                        location.getLongitude()), 13));
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0,
                 mlocationListener);
         getMap().setMyLocationEnabled(true);
         iconFactory.setStyle(IconGenerator.STYLE_GREEN);
-        addIcon(iconFactory,new LatLng(location.getLatitude(), location.getLongitude()));
+        addIcon(iconFactory, new LatLng(location.getLatitude(), location.getLongitude()));
 
-        addIcon(iconFactory,new LatLng(location.getLatitude()+0.00002, location.getLongitude()+0.0005));
+        addIcon(iconFactory, new LatLng(location.getLatitude() +
+                0.00002, location.getLongitude() + 0.0005));
 
+        getMap().setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+
+                return false;
+            }
+        });
     }
 
 
-    private void addIcon(IconGenerator iconFactory,  LatLng position) {
+    private void addIcon(IconGenerator iconFactory, LatLng position) {
         MarkerOptions markerOptions = new MarkerOptions().
                 icon(BitmapDescriptorFactory.fromResource(R.drawable.taxi)).
                 position(position).
