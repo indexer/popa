669dbd422eba9a2cc447d64bf2808bd46a3b70b1
indexer <yemonkyaw007@gmail.com>
2014-05-17 09:07:10 -0400

    camera listener

--- app/src/main/java/com/indexer/hellotaxi/app/Listener/newPhotoListener.java
+++ app/src/main/java/com/indexer/hellotaxi/app/Listener/newPhotoListener.java
@@ -3,26 +3,22 @@ package com.indexer.hellotaxi.app.Listener;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.net.Uri;
-import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.Toast;
 
-import com.indexer.hellotaxi.app.R;
-
-import java.io.File;
 
 /**
  * Created by yemonkyaw on 5/17/14.
  */
-public class newPhotoListener implements View.OnClickListener{
+public class newPhotoListener implements View.OnClickListener {
     Activity mActivity;
+    Uri mImageUri;
+    private String selectedImagePath;
 
     public newPhotoListener(Activity activity) {
         mActivity = activity;
+
     }
 
     @Override
@@ -48,20 +44,9 @@ public class newPhotoListener implements View.OnClickListener{
                 new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+                        if (which == 0) {
 
-                        if(which == 0){
-                            Intent takePhoto = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-                            File photo = null;
-                            try
-                            {
-                                // place where to store camera taken picture
-                            }
-                            catch(Exception e)
-                            {
-                            }
-                        }else if(which == 1){
-                            Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
-                            photoPickerIntent.setType("image/*");
+                        } else if (which == 1) {
                         }
 
                         dialog.dismiss();
@@ -70,7 +55,4 @@ public class newPhotoListener implements View.OnClickListener{
         );
         builderSingle.show();
     }
-
-
-
 }
