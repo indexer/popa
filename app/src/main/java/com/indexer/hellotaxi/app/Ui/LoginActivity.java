591e274fac55cfaeb3fdb33033aa1e46678c7e10
indexer <yemonkyaw007@gmail.com>
2014-06-21 04:24:17 -0400

    login acitiviy update

--- app/src/main/java/com/indexer/hellotaxi/app/Ui/LoginActivity.java
+++ app/src/main/java/com/indexer/hellotaxi/app/Ui/LoginActivity.java
@@ -3,17 +3,27 @@ package com.indexer.hellotaxi.app.Ui;
 import android.app.Activity;
 import android.view.MenuItem;
 import com.indexer.hellotaxi.app.R;
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 
 @EActivity(R.layout.activity_login)
 public class LoginActivity extends Activity {
+
+  @AfterViews void showLoginUi() {
+    getActionBar().setDisplayHomeAsUpEnabled(true);
+    getActionBar().setHomeButtonEnabled(true);
+  }
 
   @Override
   public boolean onOptionsItemSelected(MenuItem item) {
     // Handle action bar item clicks here. The action bar will
     // automatically handle clicks on the Home/Up button, so long
     // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-    return id == R.id.action_settings || super.onOptionsItemSelected(item);
+    switch (item.getItemId()) {
+      case android.R.id.home:
+        this.onBackPressed();
+        return false;
+    }
+    return false;
   }
 }
