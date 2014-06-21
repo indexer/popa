dccf3380caf310e5ccd94b1b64867af2bca4ff52
indexer <yemonkyaw007@gmail.com>
2014-06-21 05:07:15 -0400

    add okhttp and ApiModule

---
+++ app/src/main/java/com/indexer/hellotaxi/app/Module/ApiModule.java
@@ -0,0 +1,34 @@
+package com.indexer.hellotaxi.app.Module;
+
+import com.squareup.okhttp.OkHttpClient;
+import dagger.Module;
+import dagger.Provides;
+import javax.inject.Singleton;
+import retrofit.Endpoint;
+import retrofit.Endpoints;
+import retrofit.RestAdapter;
+import retrofit.client.Client;
+import retrofit.client.OkClient;
+
+/**
+ * Created by yemonkyaw on 6/21/14.
+ */
+@Module(library = true , complete = false)
+public class ApiModule {
+  public static final String apiServer = "www.google.com";
+
+  @Provides @Singleton Client provideClient(OkHttpClient client) {
+    return new OkClient(client);
+  }
+
+  @Provides @Singleton Endpoint provideEndPoint() {
+    return Endpoints.newFixedEndpoint(apiServer);
+  }
+
+  @Provides @Singleton RestAdapter provideRestAdapter(Endpoint endpoint, Client client) {
+    return new RestAdapter.Builder() //
+        .setClient(client) //
+        .setEndpoint(endpoint) //
+        .build();
+  }
+}
