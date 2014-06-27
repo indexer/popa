25b1eec003fb7670cf15d2d5cc2b125d2758c3e0
indexer <yemonkyaw007@gmail.com>
2014-06-27 05:40:00 -0400

    check location change work or not

--- app/src/main/java/com/indexer/hellotaxi/app/Listener/mlocationListener.java
+++ app/src/main/java/com/indexer/hellotaxi/app/Listener/mlocationListener.java
@@ -3,6 +3,7 @@ package com.indexer.hellotaxi.app.Listener;
 import android.location.Location;
 import android.location.LocationListener;
 import android.os.Bundle;
+import android.util.Log;
 
 /**
  * Created by yemonkyaw on 5/10/14.
@@ -12,7 +13,7 @@ public class mlocationListener implements LocationListener {
 
   @Override
   public void onLocationChanged(Location location) {
-
+    Log.e("Chage Location","Current"+location.getLatitude()+location.getLongitude());
   }
 
   @Override
